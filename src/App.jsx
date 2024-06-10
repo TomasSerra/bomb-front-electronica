213e7bb2caba5439efb1de4189e6750e68e0cac7
TomasSerra <tomi.serra@gmail.com>
2024-06-10 02:00:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import './App.scss';
+import { useEffect, useState } from 'react';
+import Instructions from './instrucciones.jpg';
+
+function App() {
+  const [bombName, setBombName] = useState('');
+  const [leaderboard, setLeaderboard] = useState([{dispositive: 'Bomb 4', time: 31}, {dispositive: 'Bomb 1', time: 43}, {dispositive: 'Bomb 3', time: 64}]);
+
+  const startGame = async () => {
+    try {
+      const res = await fetch('http://100.28.74.43:8081/startgame', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({dispositive: bombName})
+      });
+
+      if(res.ok){
+        alert('Game started');
+        setBombName('')
+      }
+      else{
+        alert('Could not start game');
+      }
+
+    }catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getLeaderboard();
+  }, []);
+
+  const getLeaderboard = async () => {
+    try {
+      const res = await fetch('http://100.28.74.43:8081/leaderboard', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const data = await res.json();
+      if(res.ok){
+        setLeaderboard(data);
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="page">
+      <div className="col1">
+        <div className="input-container">
+          <input type="text" value={bombName} placeholder='Bomb Name' onChange={(e) => setBombName(e.target.value)} />
+          <button className="button" onClick={startGame}>Start Game</button>
+        </div>
+
+        <div className='leaderboard'>
+          <h2>Best times</h2>
+          <ul>
+            {leaderboard.map((item, index) => (
+              <div className='item' key={index}>{index+1}. <b>{item.dispositive}</b>: {item.time} seconds</div>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="col2">
+        <img src={Instructions} />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
